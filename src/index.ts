c92d7f74bd5cba8b18b1b7732db3d0deb7ac2d6f
happou31 <happo31@gmail.com>
2018-11-17 16:34:01 +0900

    add: working.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import puppeteer from "puppeteer";
+import mkdirp from "mkdirp";
+import path from "path";
 
 const shoutaNoSushiIndexUrl = "https://www.sukima.me/book/title/rigths0000003/";
+
+const saveRootDir = path.join("./dist");
 
 (async () => {
     console.log("starting...");
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ["--lang=ja,en-US,en"]
     });
     const page = await browser.newPage();
@@ -24,27 +28,30 @@ const shoutaNoSushiIndexUrl = "https://www.sukima.me/book/title/rigths0000003/";
         );
 
         // テスト用に1ページだけ
-        await getEpisodeScreenshots(browser, links[0]);
+        // await takeEpisodeScreenshots(browser, links[0]);
 
+        let index = 0;
         // 本番
-        // for (const url of links) {
-        //     // 負担を掛けないように3秒待つ
-        //     await page.waitFor(3000);
-        //     await getEpisodeScreenshots(browser, url);
-        // }
+        for (const url of links) {
+            await takeEpisodeScreenshots(browser, url, index);
+            ++index;
+        }
     }
 
     await browser.close();
 })();
 
-async function getEpisodeScreenshots(
+async function takeEpisodeScreenshots(
     browser: puppeteer.Browser,
     url: string,
+    directoryPrefixIndex: number,
     size?: { witdh: number; height: number }
 ) {
     const page = await browser.newPage();
     if (size) {
         await page.setViewport({ width: size.witdh, height: size.height });
+    } else {
+        await page.setViewport({ width: 768, height: 1210 });
     }
 
     await page.goto(url, { waitUntil: "domcontentloaded" });
@@ -56,9 +63,56 @@ async function getEpisodeScreenshots(
     }
 
     // ページ上に掛かっているコントローラーを消す
-    const rightBlankPage = await page.$("#canvas_blank_before_1");
+    const rightBlankPage = await page.$("#canvas_1");
 
     if (rightBlankPage) {
         await rightBlankPage.click();
     }
+
+    // コントローラーが消えるまで待つ
+    await page.waitFor(500);
+
+    const fullTitle = await page.title();
+
+    const title = fullTitle.split("｜")[0];
+
+    console.log(title);
+
+    const comicPath = path.join(
+        saveRootDir,
+        `${directoryPrefixIndex}_${title}`
+    );
+
+    mkdirp.sync(comicPath);
+
+    // ページ番号のIDは 1 オリジン
+    let pageIndex = 1;
+    while (true) {
+        const divId = `#canvas_${pageIndex}`;
+
+        const canvasIncludeDiv = await page.$(divId);
+
+        if (canvasIncludeDiv) {
+            const filePath =
+                path.join(comicPath, pageIndex.toString()) + ".png";
+            console.log(`   page:${filePath}`);
+            await canvasIncludeDiv.screenshot({
+                path: filePath,
+                type: "png"
+            });
+        } else {
+            // element が取れなかったら終了する
+            break;
+        }
+
+        const nextBtn = await page.$("#btn-next");
+        if (nextBtn) {
+            await nextBtn.click();
+        }
+        // ページが切り替わるまで待つ
+        await page.waitFor(200);
+        pageIndex += 1;
+    }
+
+    return await page.close();
 }
